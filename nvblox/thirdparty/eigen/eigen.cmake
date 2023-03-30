e1ce871baf4f728474ada66131b7405bb1eb9a9c
amillane <amillane@nvidia.com>
2023-03-30 10:47:30 +0200

    Remove eigen sha hash to account for a gitlab bug.

--- nvblox/thirdparty/eigen/eigen.cmake
+++ nvblox/thirdparty/eigen/eigen.cmake
@@ -4,7 +4,6 @@ ExternalProject_Add(
     ext_eigen
     PREFIX eigen
     URL https://gitlab.com/libeigen/eigen/-/archive/3.4.0/eigen-3.4.0.tar.gz
-    URL_HASH SHA256=8586084f71f9bde545ee7fa6d00288b264a2b7ac3607b974e54d13e7162c1c72
     UPDATE_COMMAND ""
     CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
 )
