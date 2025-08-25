3ec946cf541782fd7be886ff77707842e9a1507e
David Tingdahl <dtingdahl@nvidia.com>
2025-08-25 11:39:47 +0200

    Upgrade sqlite to v3.50.4

--- nvblox/thirdparty/sqlite/sqlite.cmake
+++ nvblox/thirdparty/sqlite/sqlite.cmake
@@ -3,16 +3,18 @@ if (USE_SYSTEM_SQLITE3)
 else()
 include(FetchContent)
 
+# Download the amalgamation version of sqlite that contains the source code in
+# one massive source file.
 FetchContent_Declare(
   ext_sqlite3
   OVERRIDE_FIND_PACKAGE
   SYSTEM
-  URL https://github.com/alex85k/sqlite3-cmake/archive/refs/tags/v3.24.0.tar.gz
-  URL_HASH MD5=15ab81bf8cfbdb9a9a3b3abd38c7598b
-)
+  URL https://sqlite.org/2025/sqlite-amalgamation-3500400.zip
+  URL_HASH MD5=440abd85c5ee3297dd388ade51fec0cc)
 
 FetchContent_MakeAvailable(ext_sqlite3)
-target_include_directories(sqlite3 PUBLIC ${sqlite3_SOURCE_DIR}/src)
+add_library(sqlite3 ${ext_sqlite3_SOURCE_DIR}/sqlite3.c)
+target_include_directories(sqlite3 PUBLIC ${ext_sqlite3_SOURCE_DIR})
 
 # Apply nvblox compile options to exported targets
 set_nvblox_compiler_options_nowarnings(sqlite3)
