4f57addd73a2d0ffe213ad3197fb4b2c9a2efaed
David Tingdahl <dtingdahl@nvidia.com>
2025-12-02 18:05:35 +0100

    privileged flag for docker run

--- ci/ci_utils.py
+++ ci/ci_utils.py
@@ -315,7 +315,7 @@ class TestBase(ABC):
     def run(self) -> None:
         """Build image and run command inside it"""
         self.image().build()
-        docker_cmd = ['docker', 'run', '--rm', self.image().image_name()]
+        docker_cmd = ['docker', 'run', '--rm', '--privileged', self.image().image_name()]
         cwd = self.get_cwd()
         cmd = self.get_command()
         full_cmd = docker_cmd + ['bash', '-c'] + [f'cd {cwd} && {cmd}']
