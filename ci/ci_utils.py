c261ca2956e5e6c0d5fb9167510bf2ae92970c52
David Tingdahl <dtingdahl@nvidia.com>
2026-01-16 13:11:01 +0100

    update l4t image to match CI machine

--- ci/ci_utils.py
+++ ci/ci_utils.py
@@ -345,7 +345,7 @@ class OsImage(DockerImage):
         },
         Platform.JETPACK_6: {
             CudaVersion.CUDA_12: {
-                UbuntuVersion.UBUNTU_22: 'nvcr.io/nvidia/l4t-jetpack:r36.3.0'
+                UbuntuVersion.UBUNTU_22: 'nvcr.io/nvidia/l4t-jetpack:r36.4.0'
             }
         },
     }
