97fdb3de2a97ccb444baf7219088b45dcc9774cb
David Tingdahl <dtingdahl@nvidia.com>
2026-01-30 11:46:17 +0100

    refactoring torch install helper

--- docker/torch_install_helper.py
+++ docker/torch_install_helper.py
@@ -14,32 +14,44 @@ import subprocess
 import re
 import platform
 import argparse
+from dataclasses import dataclass
 
-# pylint: disable=line-too-long
-X86_CUDA_VERSION_TO_PYTORCH_PIP_URL = {
-    # Latest version of pytorch compatible with CUDA 11
-    '11': '--index-url https://download.pytorch.org/whl/cu118/torch/ --extra-index-url https://pypi.org/simple torch==2.7.1',
-    # Default pypi version uses CUDA 12
-    '12': 'torch==2.9.1',
-    '13': '--index-url https://download.pytorch.org/whl/cu130/torch/ --extra-index-url https://pypi.org/simple torch==2.9.1',
-}
 
-# We currently only support Jetpack 6 (Cuda12) for jetson. These are nvidia-hosted builds of JP6 wheels.
-JETSON_CUDA_VERSION_TO_TORCH = {
-    '12': {
-        'url': 'https://nvidia.box.com/shared/static/mp164asf3sceb570wvjsrezk1p4ftj8t.whl',
-        'filename': 'torch-2.3.0-cp310-cp310-linux_aarch64.whl'
-    },
-}
+@dataclass
+class PytorchVersion:
+    """Store information of a pythorch version."""
+    platform: str
+    cuda_version: str
 
-# On Jetson we also need to explicitly install torchvision.
-JETSON_CUDA_VERSION_TO_TORCHVISION = {
-    '12': {
-        'url': 'https://nvidia.box.com/shared/static/xpr06qe6ql3l6rj22cu3c45tz1wzi36p.whl',
-        'filename': 'torchvision-0.18.0-cp310-cp310-linux_aarch64.whl'
-    },
-}
-# pylint: enable=line-too-long
+    pytorch_version: str
+    pytorch_url: str
+    torchvision_url: str | None = None    # Only needed for jetson.
+
+
+# List of supported pytorch versions in this project.
+PYTORCH_VERSIONS = [
+    PytorchVersion(platform='x86',
+                   cuda_version='11',
+                   pytorch_version='2.7.1',
+                   pytorch_url='https://download.pytorch.org/whl/cu118/torch/'),
+    PytorchVersion(platform='x86',
+                   cuda_version='12',
+                   pytorch_version='2.9.1',
+                   pytorch_url='https://download.pytorch.org/whl/cu128/torch/'),
+    PytorchVersion(platform='x86',
+                   cuda_version='13',
+                   pytorch_version='2.9.1',
+                   pytorch_url='https://download.pytorch.org/whl/cu130/torch/'),
+    PytorchVersion(
+        platform='aarch64',
+        cuda_version='12',
+        pytorch_version='2.3.0',
+        pytorch_url='https://nvidia.box.com/shared/static/xpr06qe6ql3l6rj22cu3c45tz1wzi36p.whl',
+        torchvision_url=
+    # pylint: disable=line-too-long
+        'https://pypi.jetson-ai-lab.io/jp6/cu126/+f/d5b/caaf709f11750/torchvision-0.24.1-cp310-cp310-linux_aarch64.whl'
+    ),
+]
 
 
 def get_cuda_version() -> str:
@@ -52,60 +64,45 @@ def get_cuda_version() -> str:
     return match.group(1).split('.')[0]
 
 
-def install_pytorch_x86() -> None:
-
-    cuda_version = get_cuda_version()
-    if cuda_version not in X86_CUDA_VERSION_TO_PYTORCH_PIP_URL:
-        raise ValueError(f'Unsupported CUDA version: {cuda_version}')
-
-    pytorch_install = X86_CUDA_VERSION_TO_PYTORCH_PIP_URL[cuda_version]
-    print(f'installing pytorch from {pytorch_install}')
-
-    script = f"""
-    set -ex
-    umask 000
-    . /opt/venv/bin/activate
-    python3 -m pip install --ignore-installed --upgrade pip --no-cache-dir
-    python3 -m pip install --no-cache-dir {pytorch_install}
-    """
-
-    subprocess.run(script, shell=True, check=True)
+def get_pytorch_version_for_this_machine() -> PytorchVersion:
+    """Get the pytorch version for the current system or None if not supported."""
+    return next(pytorch_version for pytorch_version in PYTORCH_VERSIONS
+                if pytorch_version.platform == platform.machine()
+                and pytorch_version.cuda_version == get_cuda_version())
 
 
-def install_pytorch_jetson() -> None:
+def install_pytorch_if_supported_for_this_machine() -> None:
 
-    cuda_version = get_cuda_version()
-    if cuda_version not in JETSON_CUDA_VERSION_TO_TORCH:
-        print('warning: Unsupported CUDA version: {cuda_version}. Skipping pytorch installation.')
+    pytorch_version = get_pytorch_version_for_this_machine()
+    if pytorch_version is None:
+        cuda_version = get_cuda_version()
+        print(f'Warning: pytorch not supported on this system: '
+              f'{platform.machine()} with cuda version: {cuda_version}')
         return
 
-    torch_url = JETSON_CUDA_VERSION_TO_TORCH[cuda_version]['url']
-    torch_filename = JETSON_CUDA_VERSION_TO_TORCH[cuda_version]['filename']
-    torchvision_url = JETSON_CUDA_VERSION_TO_TORCHVISION[cuda_version]['url']
-    torchvision_filename = JETSON_CUDA_VERSION_TO_TORCHVISION[cuda_version]['filename']
-
     script = f"""
     set -ex
     umask 000
     . /opt/venv/bin/activate
     python3 -m pip install --ignore-installed --upgrade pip --no-cache-dir
-    wget {torch_url} -O  /{torch_filename}
-    pip install /{torch_filename}
-    rm /{torch_filename}
-    wget {torchvision_url} -O /{torchvision_filename}
-    pip install /{torchvision_filename}
-    rm /{torchvision_filename}
+    python3 -m pip install --no-cache-dir {PytorchVersion.pytorch_url}
     """
+
+    if PytorchVersion.torchvision_url is not None:
+        script += f"""
+        python3 -m pip install --no-cache-dir {PytorchVersion.torchvision_url}
+        """
+
     subprocess.run(script, shell=True, check=True)
 
 
-def install_nvblox_torch() -> None:
+def install_nvblox_torch_if_supported_for_this_machine() -> None:
 
-    cuda_version = get_cuda_version()
-    supported_cuda_versions = list(JETSON_CUDA_VERSION_TO_TORCH.keys()
-                                   | X86_CUDA_VERSION_TO_PYTORCH_PIP_URL.keys())
-    if cuda_version not in supported_cuda_versions:
-        print(f'warning: Unsupported CUDA version: {cuda_version}. Skipping nvblox torch install.')
+    pytorch_version = get_pytorch_version_for_this_machine()
+    if pytorch_version is None:
+        cuda_version = get_cuda_version()
+        print(f'Warning: nvblox torch not supported on this system: '
+              f'{platform.machine()} with cuda version: {cuda_version}')
         return
 
     script = """
@@ -113,12 +110,10 @@ def install_nvblox_torch() -> None:
     umask 000
     . /opt/venv/bin/activate
     python3 -m pip install --ignore-installed --upgrade pip --no-cache-dir
+    # Need to force the torch version to prevent accidental upgrades.
+    pip install /nvblox/nvblox_torch/ "torch=={pytorch_version.pytorch_version}"'
     """
-    # Need to force the torch version for cuda 11 to prevent upgrade.
-    if cuda_version == '11':
-        script += 'pip install /nvblox/nvblox_torch/ "torch==2.7.1"'
-    else:
-        script += 'pip install /nvblox/nvblox_torch/'
+
     subprocess.run(script, shell=True, check=True)
 
 
@@ -147,16 +142,10 @@ def main() -> None:
      CUDA version (12 at the time of writing). Therefore we need this custom install script.
     """
     args = parse_args()
-
     if args.install_pytorch_if_supported:
-        if platform.machine() == 'x86_64':
-            install_pytorch_x86()
-        elif platform.machine() == 'aarch64':    # Jetson
-            install_pytorch_jetson()
-        else:
-            raise ValueError(f'Unsupported system: {platform.machine()}')
-    elif args.install_nvblox_torch_if_supported:
-        install_nvblox_torch()
+        install_pytorch_if_supported_for_this_machine()
+    if args.install_nvblox_torch_if_supported:
+        install_nvblox_torch_if_supported_for_this_machine()
 
 
 if __name__ == '__main__':
